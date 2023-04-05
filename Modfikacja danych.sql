df6e824e7b282dbdb1c8c400446e4ce60418d8e6
agaktosia <ktosia@gmail.com>
2023-04-05 12:52:09 +0200

    dodanie modyfikacji danych

---
+++ Modfikacja danych.sql
@@ -0,0 +1,24 @@
+-- MODTFIKACJA DANYCH
+
+-- Wstawianie danych
+
+--Dodaj klienta
+INSERT INTO Klienci(Imie, Nazwisko, Wiek)
+VALUES ('Krystyna', 'Jankowiak', 30);
+
+--Dodaj do sklepu nowy produkt z telefonem, którego używasz
+INSERT INTO Produkty(Nazwa, Cena, IdKategorii)
+VALUES('Samsung S10+', 4400, (SELECT IdKategorieProduktow FROM KategorieProduktow WHERE Nazwa = 'Telefony'));
+
+-- Aktualizacja danych
+
+--Usuń komentarz do zamówienia online nr 311
+UPDATE ZakupySklepOnline
+SET Komentarz = ''
+WHERE IdZakupySklepOnline = 311;
+
+--Ustaw cenę iPhone X jako średnią cenę wszystkich iPhone
+UPDATE Produkty
+SET Cena = (SELECT AVG(Cena) FROM Produkty WHERE Nazwa LIKE '%iPhone%')
+WHERE Nazwa = 'Apple iPhone X';
+
